edcddef9cebafc75192a761d366c9571f7f46c0e
bretz032 <fernandobretz@gmail.com>
2021-08-19 09:54:37 -0300

    Implementacao Interface Pessoa 🧍

---
+++ 03_RestWithASPNET_UsingDiferentVerbs/RestWithAsp/RestWithAsp/Controllers/PersonController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithAsp.Model;
+using RestWithAsp.Services;
+
+
+
+namespace RestWithAsp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+        // Declaracao do serviço usado
+        private IPersonService _personService;
+
+        // INjecao de uma instância do Serviço de Iperson
+        // Ao criar uma instância do controlador de pessoas
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get( )
+        {
+
+             
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindByID(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(person);
+
+            }
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Post([FromBody] Person person)
+        {
+             if (person == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Ok(_personService.Create(person));
+
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Ok(_personService.Update(person));
+
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+             _personService.Delete(id);
+            if (id == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return NoContent();
+
+            }
+        }
+
+    }
+}
--- 03_RestWithASPNET_UsingDiferentVerbs/RestWithAsp/RestWithAsp/Model/Person.cs
+++ 03_RestWithASPNET_UsingDiferentVerbs/RestWithAsp/RestWithAsp/Model/Person.cs
@@ -12,7 +12,7 @@ namespace RestWithAsp.Model
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        public string Adress { get; set; }
+        public string Address { get; set; }
 
         public string Gender { get; set; }
 

a8a57e1d7e9fca06c157e30dc642bb45faf9aec7
bretz032 <fernandobretz@gmail.com>
2021-08-24 10:46:31 -0300

    Migrations 🐦

---
+++ 06_RestWithASPNET_Migrations/RestWithAsp/RestWithAsp/Repository/Implementations/PersonRepositoryImplementation.cs
@@ -0,0 +1,97 @@
+﻿using RestWithAsp.Model;
+using System.Collections.Generic;
+using System.Threading;
+using RestWithASPNETUdemy.Model.Context;
+using System.Linq;
+using System;
+
+namespace RestWithAsp.Repository.Implementations
+{
+    public class PersonRepositoryImplementation : IPersonRepository
+    {
+   
+        private PostgreSQLContext  _context;
+        
+        public PersonRepositoryImplementation(PostgreSQLContext context)
+        {
+            _context = context;
+        }
+        // Método responsável por devolver todas as pessoas
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        // Método responsável por devolver uma pessoa por ID
+        public Person FindByID(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        //Método responsável por Creta uma nova pessoa
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return person;
+        }
+        // método responsável pela atualização de uma pessoa
+        public Person Update(Person person)
+        {
+            // Verificamos se a pessoa existe no banco de dados
+            // Se não existe, retornamos uma instância de pessoa vazia
+            if (!Exists(person.Id))
+            {
+                
+                return new Person(); 
+            
+            }
+
+            // Obtenha o status atual do registro no banco de dados
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null)
+            {
+                try
+                {
+                    // set changes and save
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return person;
+        }
+
+        // Método responsável por excluir uma pessoa de um id
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+        public bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
+        }
+    }
+}

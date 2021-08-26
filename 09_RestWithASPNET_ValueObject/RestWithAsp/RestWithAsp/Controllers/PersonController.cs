5cfe36abe2ec2f732cf909b698291d9a929f572f
bretz032 <fasbretz@sga.pucminas.br>
2021-08-26 11:05:30 -0300

    Value Object 💸

---
+++ 09_RestWithASPNET_ValueObject/RestWithAsp/RestWithAsp/Controllers/PersonController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithAsp.Model;
+using RestWithAsp.Negocios;
+using RestWithASPNETUdemy.Data.VO;
+
+namespace RestWithAsp.Controllers
+{
+    [ApiController]
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+        // Declaracao do serviço usado
+        private IPersonNegocios _personService;
+
+        // INjecao de uma instância do Serviço de Iperson
+        // Ao criar uma instância do controlador de pessoas
+        public PersonController(ILogger<PersonController> logger, IPersonNegocios personService)
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindByID(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(person);
+
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+
+    }
+}
